af8a3cd33d87c76c1af248bfd1a302252e3909a3
Robert Brice <rob.a.brice@gmail.comC>
2017-04-04 16:26:38 +0100

    passing test dinosaur.offSpring

--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -9,12 +9,13 @@ beforeEach(function(){
   dino1 = new Dinosaur("Tyranosauraus Rex", 2);
 })
 
-
 it("can get name of dinosaur", function(){
-assert.strictEqual("Tyranosauraus Rex", dino1.type)
+assert.strictEqual("Tyranosauraus Rex", dino1.type);
 })
 
-
+it("can get number of off spring per year", function(){
+assert.strictEqual(2, dino1.offSpring);
+})
 
 
 
